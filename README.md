4f75c96f6cfaf3403c490f673ee018f833020621
Oleg Kovalenko <kn1feR26rus@yandex.ru>
2021-02-02 21:58:00 +0300

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# Demo-Test project
+_____________________
+
+:white_check_mark: need to install live server
---
+++ src/pages/index.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+deleteBtn.addEventListener('click', () => {
+    modal.classList.add('modal_opened')
+})
+
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('modal_opened')
+})
+
+cancelBtn.addEventListener('click', () => {
+    modal.classList.remove('modal_opened')
+})
+
+
+
+function newTask() {
+    
+    let title = inputTask.value;
+    const correct = document.getElementById('#correct');
+    const del = document.getElementById('#delete');
+
+
+}
+*/
